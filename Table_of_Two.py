a9a6c414f74713c1a160521735777dbdf2f93824
Mahesh Kakde <maheshkakde100@gmail.com>
2022-07-09 23:23:09 +0530

    Table of Two get modified Jenkins Operations

--- Table_of_Two.py
+++ Table_of_Two.py
@@ -5,3 +5,13 @@ num=2
 for a in range(1,11):    
 
    print(num,'x',a,'=',num*a)  
+
+print("\n")
+
+#This Program to print Table of two
+
+print("Table of 3 is : ")  
+num=2
+for a in range(1,11):    
+
+   print(num,'x',a,'=',num*a)  
